10b4498444489a16d1bd079158b3b8f24d5b1aeb
llerqw <lodyginavaleria337@gmail.com>
2025-11-04 19:32:57 +0300

    AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
 });
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
 
+});
 Auth::routes();
