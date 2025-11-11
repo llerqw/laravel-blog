9ced5b6d7ffe42ec4eb05863731dcb22a496ee19
llerqw <lodyginavaleria337@gmail.com>
2025-11-11 18:54:04 +0300

    14. Основа CRUD для постов

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -7,6 +7,15 @@
         <nav class="mt-2">
             <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
+                    <a href="{{route('admin.post.index')}}" class="nav-link">
+                        <i class="nav-icon far fa-clipboard"></i>
+                        <p>
+                            Посты
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
                     <a href="{{route('admin.category.index')}}" class="nav-link">
                         <i class="nav-icon far fa-image"></i>
                         <p>
@@ -22,6 +31,7 @@
                         </p>
                     </a>
                 </li>
+
             </ul>
         </nav>
     </div>
