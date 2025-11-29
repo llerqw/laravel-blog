4579be8a98eb711547a98d1e55170fbe0de08d78
llerqw <lodyginavaleria337@gmail.com>
2025-11-29 11:08:33 +0300

    28. Отправление письма с подтверждением учетной записи Email verification

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
 use App\Mail\User\PasswordMail;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
@@ -18,9 +19,12 @@ class StoreController extends Controller
         $data = $request->validated();
         $password = Str::random(10);
         $data['password'] = Hash::make($password);
-        User::firstOrCreate(['email' => $data['email']], $data);
-        Mail::to($data['email'])->send(new PasswordMail($password));
+        $user = User::firstOrCreate(['email' => $data['email']], $data);
 
+        Mail::to($data['email'])->send(new PasswordMail($password));
+        // Всегда ждем 10 секунд перед отправкой письма
+        sleep(10);
+        event(new Registered($user));
         return redirect()->route('admin.user.index');
     }
 }
