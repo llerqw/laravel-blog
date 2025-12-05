83a66a70003f99ebd9e328f5075b84ccd43bf33a
llerqw <lodyginavaleria337@gmail.com>
2025-12-05 09:54:01 +0300

    35. Вывод ленты постов. Популярные посты. пагинация

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,8 +14,17 @@ class Post extends Model
     protected $table = 'posts';
     protected $guarded = false;
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
     public function tags()
     {
         return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
+    }
+
+    public function likedUsers()
+    {
+        return$this->belongsToMany(User::class, 'post_user_likes', 'post_id', 'user_id');
     }
 }
